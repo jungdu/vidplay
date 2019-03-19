820a603db2573ea850ccca62dbffb827fbbc29c7
jungdu <jungdujang@gmail.com>
2019-03-19 11:11:41 +0900

    fetch 경로 환경설정 변경

--- src/apis/vid.js
+++ src/apis/vid.js
@@ -2,5 +2,5 @@ import axios from "axios";
 import { serverUrl } from "../config";
 
 export default axios.create({
-  baseURL: `${serverUrl}/api`
+  baseURL: `${serverUrl}/`
 });
--- src/config.js
+++ src/config.js
@@ -1,1 +1,5 @@
-export const serverUrl = "http://localhost:4000";
+export const serverUrl =
+  "https://mw5sw6bvqi.execute-api.ap-northeast-2.amazonaws.com/dev";
+
+export const bucketUrl =
+  "https://s3.ap-northeast-2.amazonaws.com/vidplay.abouthorn.com/";
