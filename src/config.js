5021435870415607a9c6f8c484bf90de89d008da
jungdu <jungdujang@gmail.com>
2019-03-19 11:40:16 +0900

    api gateway 도메인 등록 후 설정 변경

--- src/config.js
+++ src/config.js
@@ -1,5 +1,4 @@
-export const serverUrl =
-  "https://mw5sw6bvqi.execute-api.ap-northeast-2.amazonaws.com/dev";
+export const serverUrl = "https://vidplayapi.abouthorn.com/";
 
 export const bucketUrl =
   "https://s3.ap-northeast-2.amazonaws.com/vidplay.abouthorn.com/";
