88040aa63dd24b92acec62e3be6a6f920fc883ca
jungdu <jungdujang@gmail.com>
2019-04-05 10:43:27 +0900

    자막파일 필터링

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,7 +3,8 @@ import { FETCH_VIDS, SELECT_VID, OPEN_LIST, CLOSE_LIST } from "./types";
 
 export const fetchVids = () => async dispatch => {
   const response = await axios.get("/videos");
-  dispatch({ type: FETCH_VIDS, payload: response.data.videos });
+  const videos = response.data.videos.filter(elem => !/vtt$/.exec(elem));
+  dispatch({ type: FETCH_VIDS, payload: videos });
 };
 
 export const selectVid = video => {
